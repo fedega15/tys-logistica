d9bc72d8e7b8bcd1747e50dd41489f3a2720f040
fedega15 <federico.galassi9@gmail.com>
2023-03-14 11:55:08 -0300

    14-3-23

--- src/components/Breadcrumbs/breadcrubm.jsx
+++ src/components/Breadcrumbs/breadcrubm.jsx
@@ -4,12 +4,18 @@ import { Link } from 'react-router-dom';
 const Breadcrumbs = () => {
   return (
     <Breadcrumb class='d-flex justify-content-start bg-secondary text-white' style= {{height: '42px', paddingLeft: "10px"}}>
+      
+      <Link style={{textDecoration:"none"}} to="/login">
+        <p class="text-light fw-semibold "> Inicia sesion /</p>
+      </Link>
+      
       <Link to="/" style={{textDecoration:"none"}}>
-        <p class="text-light fw-semibold">Home / </p>
+        <p class="text-light fw-semibold">/ Lista de camiones / </p>
       </Link>
       <br />
-      <Link style={{textDecoration:"none"}} to="/login">
-        <p class="text-light fw-semibold ">/ Login</p>
+     
+      <Link style={{textDecoration:"none"}} to="/AgregarCamiones">
+        <p class="text-light fw-semibold ">/ Agrega tu camion</p>
       </Link>
 
     </Breadcrumb>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -7,32 +7,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
     const Login =( ) => {
 
-        const [credenciales, setCredenciales] = useState ({
-            useremail: null ,           
-            password: null,  
-        });
+        // const [credenciales, setCredenciales] = useState ({
+          //  useremail: null ,           
+           // password: null,  
+       // });
         
-        const [errores, setErrores] = useState({
-            'useremail': null,
-            'password': null,
-        })
+       // const [errores, setErrores] = useState({
+        //    'useremail': null,
+        //    'password': null,
+       // })
 
 
-        const handleChange = async () => {
-            try  {
-                const [errors, correct] = validacionSignUp(credenciales)
-                setErrores(errors)
-                if (correct ) {
-                    const api_response = await newUser(credenciales)
-                        if(api_response.status === 200){
-                            const {data} = api_response.
-                            setCredenciales (data)
-                            console.log(credenciales)
-                        }
-                    }console.log(errors, correct)
-                }catch (error) {
-                    console.log("error en registro")
-                }
+      //  const handleChange = async () => {
+        //    try  {
+         //       const [errors, correct] = validacionSignUp(credenciales)
+         //       setErrores(errors)
+          //      if (correct ) {
+         //           const api_response = await newUser(credenciales)
+          //              if(api_response.status === 200){
+           //                 const {data} = api_response.
+           //                 setCredenciales (data)
+            //                console.log(credenciales)
+            //            }
+           //         }console.log(errors, correct)
+            //    }catch (error) {
+           //         console.log("error en registro")
+           //     }
     return (
         <div className='container'>
         <Formik > 
@@ -46,9 +46,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
                                 <label htmlFor="useremail">Email</label>
                                 <Field 
                                     className="form-control" 
-                                    onChange = {value => setCredenciales ({ ...credenciales, "useremail": value})}
+                                //    onChange = {value => setCredenciales ({ ...credenciales, "useremail": value})}
                                     placeholder="tys@ejemplo.com"
-                                    erroresMessage={errores.useremail}/>
+                               //     erroresMessage={errores.useremail}
+                               />
                             </div>
                         </div>
 
@@ -58,9 +59,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
                                 <Field 
                                     className="form-control" 
                                     name="password"
-                                    onChange = {value => setCredenciales({...credenciales, 'password': value})}
+                                //    onChange = {value => setCredenciales({...credenciales, 'password': value})}
                                     placeholder="***********"
-                                    errorMessage={errores.password}
+                                //    errorMessage={errores.password}
                                     />
 
                             </div>
@@ -69,7 +70,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
                     <div className="form-group">
                         <button 
                             type="submit" 
-                            onClick={handleChange} >
+                      //      onClick={handleChange} 
+                      >
                             Enviar
                         </button>
                     </div>
@@ -82,5 +84,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
         </div>
     )};
 
-    }
+  //  }
 export default Login;
